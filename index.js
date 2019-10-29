8c1ddb59d91a636bed6ab7db7ef7904974ca4b8d
Ivan da Silveira <ivandasilveira@gmail.com>
2019-10-29 14:47:13 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
+const inlineDetection = [
+	'module.exports =',
+	'export default',
+];
+
 module.exports = function loader(source) {
+
+	// remove inline code if present.
+	inlineDetection.forEach((detection) => {
+		if(source.indexOf(detection) == 0) {
+			source = source.substr(detection.length);
+			source = source.trim();
+			if(source[source.length - 1] == ';') {
+				source = source.substring(0, source.length - 1);
+			}
+			source = source.substring(1, source.length - 1);
+		}
+	});
+
 	return `
 		import { svg } from 'lit-element';
 		export default svg\`${ source }\`;
